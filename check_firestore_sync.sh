1dbcc82735f5cadae1bbbf949359ce811c966655
Divyaprakash <divi242006@gmail.com>
2026-03-28 10:32:37 +0530

    chore: remove junk scripts and patches accidentally merged from PR 24 and others

--- check_firestore_sync.sh
+++
@@ -1,1 +0,0 @@
-cat ./app/src/main/java/com/pulse/data/sync/FirestoreSyncWorker.kt | grep -A 20 -B 10 "val toInsert"
--- fix_di_repo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/AppDatabase.MIGRATION_13_14)/AppDatabase.MIGRATION_13_14, AppDatabase.MIGRATION_14_15)/g' app/src/main/java/com/pulse/di/AppModule.kt
